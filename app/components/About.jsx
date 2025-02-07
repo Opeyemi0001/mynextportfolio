3b30bcf0b646c38b4ebce01685469bb6ac47d52e
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-02-07 18:03:46 +0100

    feat: Services

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
           </p>
 
           <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl' >
-            {infoList.map(({icon, iconDark, title, description}, index) => (
+            {infoList.map(({ icon, iconDark, title, description }, index) => (
               <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black' key={index}>
                 <Image src={icon} alt={title} className='w-7 mt-3' />
                 <h3 className='my-4 font-semibold text-gray-700'> {title} </h3>
@@ -32,7 +32,7 @@ const About = () => {
           <h4 className='my-6 text-gray-700 font-Ovo'>Tools I use</h4>
 
           <ul className='flex items-center gap-3 sm:gap-5'>
-            {toolsData.map((tool,index)=>(
+            {toolsData.map((tool, index) => (
               <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' key={index}>
                 <Image src={tool} alt='Tool' className='w-5 sm:w-7' />
               </li>
