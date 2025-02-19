8fb76e6a82f0a7595935ffa5de4e8dc783431cfb
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-02-19 13:29:28 +0100

    update on infoList

--- assets/assets.js
+++ assets/assets.js
@@ -166,15 +166,15 @@ export const infoList = [
   {
     icon: assets.code_icon,
     iconDark: assets.code_icon_dark,
-    title: "Languages",
-    description: "HTML, CSS, JavaScript, React Js, Next Js",
+    title: "My Stacks",
+    description: "JavaScript, React Js, Next Js, TailwindCSS, Node.js, Express.js, MongoDB.",
   },
   // { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
   {
     icon: assets.project_icon,
     iconDark: assets.project_icon_dark,
     title: "Projects",
-    description: "Built more than 4 projects",
+    description: "Built more than 4 projects.",
   },
 ];
 
